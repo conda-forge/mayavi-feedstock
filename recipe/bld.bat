15197756a94fd2a42ce61957d5deeb35555f40b5
Eric Larson <larson.eric.d@gmail.com>
2022-12-21 08:31:14 -0500

    FIX: Manual

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
 del tvtk\src\array_ext.c
+cython tvtk\src\array_ext.pyx
 %PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ Xvfb :1 -screen 0 1280x1024x24 -auth localhost &
 export DISPLAY=:1
 # This rm line can be removed once array_ext.c is 3.11-compatible
 rm tvtk/src/array_ext.c
+cython tvtk/src/array_ext.pyx
 $PYTHON -m pip install . --no-deps -vv
 
 unamestr=`uname`
