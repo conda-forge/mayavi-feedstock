8644ce70d4293d2473e399b4f39e970105731c5d
Eric Larson <larson.eric.d@gmail.com>
2022-12-21 07:57:52 -0500

    FIX: Another way

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,3 @@
+del tvtk\src\array_ext.c
 %PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,11 @@
 USERNAME=$(id -u -n)
 Xvfb :1 -screen 0 1280x1024x24 -auth localhost &
 export DISPLAY=:1
+# This rm line can be removed once array_ext.c is 3.11-compatible
+rm tvtk/src/array_ext.c
 $PYTHON -m pip install . --no-deps -vv
 
 unamestr=`uname`
-if [ "$unamestr" != 'Darwin' ]; then 
-	killall -u $USERNAME Xvfb 
+if [ "$unamestr" != 'Darwin' ]; then
+	killall -u $USERNAME Xvfb
 fi
